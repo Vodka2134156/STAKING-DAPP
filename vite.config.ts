50c3e95a2d169922ad5b38e2ba7349cf59980f46
adamsirri1231 <165979399+Vodka213415@users.noreply.github.com>
2024-09-18 13:27:01 +0100

    lo

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import inject from '@rollup/plugin-inject'
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    build:
+      mode === 'production'
+        ? {
+            rollupOptions: {
+              plugins: [inject({ Buffer: ['buffer', 'Buffer'] })],
+            },
+          }
+        : undefined,
+
+    define:
+      mode === 'development'
+        ? {
+            global: {},
+          }
+        : undefined,
+  };
+});
